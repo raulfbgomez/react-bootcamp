fd04df31e8283033e67e61c56ad07dccbc92c5c9
= <=>
2019-11-27 20:04:27 -0600

    React hooks

---
+++ hooks/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Playground from './components/Playground';
+ 
+ReactDOM.render(<Playground />, document.getElementById('app'));
---
+++ hooks/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
